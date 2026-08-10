545af7215ba374ab9c1991db5cf06b40e578018a
Sylvester Roos <sylvester@sylvesterroos.com>
2026-08-10 17:03:34 +0200

    Enable latex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,20 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sylvesterroos.com",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    sitemap(),
+    icon(),
+  ],
   server: { host: true },
 
   vite: {
