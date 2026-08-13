20a860182c3c94b6eb92ae80997df5cf5fe6f4ee
Sylvester Roos <sylvester@sylvesterroos.com>
2026-08-13 15:45:09 +0200

    Formatting

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -2,5 +2,5 @@ import { getCollection } from "astro:content";
 
 export async function getPublishedBlogPosts() {
   const filterDrafts = import.meta.env.PROD;
-  return await getCollection("blog", ({ data }) => filterDrafts ? !data.draft : true);
+  return await getCollection("blog", ({ data }) => (filterDrafts ? !data.draft : true));
 }
